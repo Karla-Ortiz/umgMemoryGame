6d261989ee26697bc5da2f22fd478ad9441ebc97
Karla-Ortiz <karlaortizgc@gmail.com>
2021-09-02 08:52:50 -0600

    First commit

---
+++ src/umgmemorygame/imagenes/RutaImg.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umgmemorygame.imagenes;
+
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author KORTIZ
+ */
+public class RutaImg {
+
+    public static Image getLogoGuatex() {
+        return new Image("/umgmemory/imagenes/logo.jpg");
+    }
+}
