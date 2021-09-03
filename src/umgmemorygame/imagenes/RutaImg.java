840a76827333ec32f17a02cb6e60573ab6a4c351
Karla Ortiz <karlaortizgc@gmail.com>
2021-09-02 22:40:54 -0600

    [ADD]Ruta logo

--- src/umgmemorygame/imagenes/RutaImg.java
+++ src/umgmemorygame/imagenes/RutaImg.java
@@ -5,15 +5,37 @@
  */
 package umgmemorygame.imagenes;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import javafx.scene.image.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author KORTIZ
  */
 public class RutaImg {
+    private static final String logo = "src/corp/karby/buscaminas/images/banderaRoja.png";
+    
+    private static ImageIcon resizable(String imagen, int v) {
+        BufferedImage icono = null;
+        try {
+            BufferedImage image = ImageIO.read(new File(imagen));
+            icono = new BufferedImage(v, v, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = icono.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2.drawImage(image, 0, 0, v, v, null);
+            g2.dispose();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ImageIcon(icono);
+    }
 
-    public static Image getLogoGuatex() {
-        return new Image("/umgmemory/imagenes/logo.jpg");
+    public static ImageIcon getLogo() {
+        return resizable(logo, 35);
     }
 }
